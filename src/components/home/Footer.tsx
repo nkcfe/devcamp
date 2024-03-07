c60ee94fe75ef24e366010e84df5fd2c557259ff
nkc930604@gmail.com <nkc930604@gmail.com>
2024-03-07 20:55:00 +0900

    스타일링: "Footer" 컴포넌트 생성 및 적용"
    - Footer 스타일링 완료

---
+++ src/components/home/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="flex w-screen flex-col bg-foreground p-10 text-xs text-primary-foreground">
+      <div className="text-base">MONDAY COFFEE</div>
+      <div className="mt-4">
+        CEO: Tomlash ㅣ Business License: 000-00-00000 ㅣ Communication Sales
+        Business Report: 0000-Seoul-0000
+      </div>
+      <div>
+        Address: Seoul, Korea CS: +82 (0)0-000-0000(AM10 - PM5, Lunch PM1 - PM2,
+        Weekend and Holiday Off)
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
