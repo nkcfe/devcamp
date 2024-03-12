5afcb0f837829ca75d5b0f450ee22851a188be8a
nkc930604@gmail.com <nkc930604@gmail.com>
2024-03-13 03:26:55 +0900

    수정: "배포 오류로 fetch 함수 수정"

--- src/app/utils/fetch.ts
+++ src/app/utils/fetch.ts
@@ -6,79 +6,99 @@ import { authOptions } from './authOptions';
 import { getServerSession } from 'next-auth';
 
 export const getProducts = cache(async () => {
-  const products = await prisma.product.findMany({
-    select: {
-      productId: true,
-      name: true,
-      price: true,
-      image: true,
-      description: true,
-      category: true,
-      detail: true,
-    },
-  });
+  try {
+    const products = await prisma.product.findMany({
+      select: {
+        productId: true,
+        name: true,
+        price: true,
+        image: true,
+        description: true,
+        category: true,
+        detail: true,
+      },
+    });
 
-  if (!products) return [];
+    if (!products) return [];
 
-  return products;
+    return products;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 });
 
 export const getProduct = cache(async (id: string) => {
-  const product = await prisma.product.findUnique({
-    where: {
-      productId: id,
-    },
-    select: {
-      productId: true,
-      name: true,
-      price: true,
-      image: true,
-      description: true,
-      category: true,
-      detail: true,
-    },
-  });
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        productId: id,
+      },
+      select: {
+        productId: true,
+        name: true,
+        price: true,
+        image: true,
+        description: true,
+        category: true,
+        detail: true,
+      },
+    });
 
-  if (!product) return null;
+    if (!product) return null;
 
-  return product;
+    return product;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 });
 
 export const getCategories = cache(async () => {
-  const categories = await prisma.product.findMany({
-    select: {
-      category: true,
-    },
-  });
+  try {
+    const categories = await prisma.product.findMany({
+      select: {
+        category: true,
+      },
+    });
 
-  if (!categories) return [];
+    if (!categories) return [];
 
-  const setCategories = new Set(categories.map((item) => item.category));
+    const setCategories = new Set(categories.map((item) => item.category));
 
-  return Array.from(setCategories);
+    return Array.from(setCategories);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 });
 
 export const getCartItems = async () => {
-  const session = await getServerSession(authOptions);
-  if (!session) return { cartItems: [], totalPrice: 0 };
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) return { cartItems: [], totalPrice: 0 };
 
-  const user = await prisma.user.findUnique({
-    where: { email: session.user?.email as string },
-    select: { cartId: true },
-  });
+    const user = await prisma.user.findUnique({
+      where: { email: session.user?.email as string },
+      select: { cartId: true },
+    });
 
-  const cartId = user?.cartId;
-  const cartItems = await prisma.cartItem.findMany({
-    where: { cartId: cartId as string },
-    include: {
-      product: true,
-    },
-  });
+    const cartId = user?.cartId;
+    const cartItems = await prisma.cartItem.findMany({
+      where: { cartId: cartId as string },
+      include: {
+        product: true,
+      },
+    });
 
-  const totalPrice = cartItems.reduce(
-    (acc, item) => acc + item.product.price,
-    0,
-  );
+    const totalPrice = cartItems.reduce(
+      (acc, item) => acc + item.product.price,
+      0,
+    );
 
-  return { cartItems, totalPrice };
+    return { cartItems, totalPrice };
+  } catch (error) {
+    console.error(error);
+    return { cartItems: [], totalPrice: 0 };
+  }
 };
