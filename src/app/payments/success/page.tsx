5e209c880243230bab2829ecee37357612ee52c6
nkc930604@gmail.com <nkc930604@gmail.com>
2024-03-13 11:41:47 +0900

    수정: 배포 오류 git psuh

--- src/app/payments/success/page.tsx
+++ src/app/payments/success/page.tsx
@@ -1,18 +1,3 @@
 export default async function Page({ searchParams }: any) {
-  const secretKey = process.env.TOSS_SECRET_KEY;
-  const basicToken = Buffer.from(`${secretKey}:`, 'utf-8').toString('base64');
-
-  const payments = await fetch(
-    `https://api.tosspayments.com/v1/payments/orders/${searchParams.orderId}`,
-    {
-      headers: {
-        Authorization: `Basic ${basicToken}`,
-        'Content-Type': 'application/json',
-      },
-    },
-  ).then((res) => res.json());
-
-  const { card } = payments;
-
   return <div></div>;
 }
